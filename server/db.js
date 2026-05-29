1a4158bc4da0c630bc3e89a7ece3f30d6e29c155
reesecoch <reesecoch@users.noreply.github.com>
2026-05-29 12:26:26 -0500

    chore: use individual PG env vars instead of DATABASE_URL

--- server/db.js
+++ server/db.js
@@ -3,8 +3,12 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
